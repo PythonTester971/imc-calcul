065b4f00656f17848bd1e71e85df77b15bbe3f07
PythonTester971 <gdenisse971@outlook.fr>
2025-01-29 10:23:20 +0100

    Ajout condition changement état de la zone d'affichage de l'imc

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>IMC Calcul</title>
 </head>
-<body>
+<body class="container bg-primary text-white">
 
 <!--FORMULAIRE -->
     <h1 class="text-center">Calculez votre IMC</h1>
@@ -16,7 +16,7 @@
 
     <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">âge</span>
-        <input id="age" type="number" name="age" min="1" max="120" class="form-control" placeholder="ans" aria-label="Username" aria-describedby="basic-addon1">
+        <input id="age" type="number" name="age" min="1" max="120" class="form-control" placeholder="ans">
     </div>
 
         <!-- <label for="age">Âge : </label><br>
@@ -25,7 +25,7 @@
     
     <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">poids</span>
-        <input id="weight" type="number" name="weight" min="1" max="200" step="0.01" required class="form-control" placeholder="kilogrammes" aria-label="Username" aria-describedby="basic-addon1">
+        <input id="weight" type="number" name="weight" min="1" max="200" step="0.01" required class="form-control" placeholder="kilogrammes">
     </div>
 
         <!-- <label for="weight">Poids : </label><br>
@@ -33,7 +33,7 @@
         <span>kg<span><br><br> -->
     <div class="input-group mb-3">
         <span class="input-group-text" id="basic-addon1">poids</span>
-        <input id="height" type="number" name="height" min="1" max="3" step="0.01" required class="form-control" placeholder="mètres" aria-label="Username" aria-describedby="basic-addon1">
+        <input id="height" type="number" name="height" min="1" max="3" step="0.01" required class="form-control" placeholder="mètres">
     </div>
 
         <!-- <label for="height">Taille : </label><br>
@@ -49,32 +49,47 @@
         $age = $_POST['age'];
         $weight = $_POST['weight'];
         $height = $_POST['height']; 
+    endif;
     ?>
     
-    <div id="imc-result">
+    <div id="imc-result" class="p-2 form-control">
         <p>Votre IMC est de :
-            <div id="imc-display">
+            <div id="imc-display" class="mb-2 form-control">
                 <?php $imc = $weight/pow($height,2);?>
-                <?php echo $imc;?>
+                <?php echo round($imc,2);?>
             </div>
-            <?php endif; ?>
 
-            
-            <div id="imc-category">
             <?php 
-                if($imc > 35){
-                    echo "Vous êtes en état d'obésité sévère!";
-                } elseif ($imc > 30) {
-                    echo "Vous êtes en état d'obésité modérée !";
-                } elseif ($imc > 25) {
-                    echo "Vous êtes en surpoids !";
-                } elseif ($imc > 18.5) {
-                    echo "Vous avez une corpulence normale!";
-                } elseif ($imc <= 18.5) {
-                    echo "Vous êtes en insufisance pondérale !";
-                };
+                if($imc > 35):
             ?>
+            <div id="imc-category" class="form-control border-danger">
+                    <p class="text-danger-emphasis">Vous êtes en état d'obésité sévère!</p>
             </div>
+            <?php
+                elseif ($imc > 30):
+            ?>
+            <div id="imc-category" class="form-control border-danger-subtle">
+                    <p class="text-danger">Vous êtes en état d'obésité modérée!</p>
+            </div>
+            <?php
+                elseif ($imc > 25):
+            ?>
+            <div id="imc-category" class="form-control border-warning">
+                   <p class="text-warning-emphasis">Vous êtes en état de surpoids!</p>
+            </div>
+            <?php
+                   elseif ($imc > 18.5):
+            ?>
+            <div id="imc-category" class="form-control border-info-subtle">
+                    <p class="text-primary">Vous avez une corpulence normale!</p>
+            </div>
+            <?php
+                elseif ($imc <= 18.5) :
+            ?>
+            <div id="imc-category" class="form-control border-danger">
+                    <p class="text-danger-emphasis">Vous êtes en insuffisance pondérale!</p>
+            </div>
+    <?php endif; ?> 
     </div>
     
 
